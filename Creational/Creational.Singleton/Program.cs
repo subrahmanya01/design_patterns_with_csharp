8220eaa0a8fd19fc3439ef7f7a667b8b4712225c
Subrahmanya Bhat <subgb98@gmail.com>
2025-07-20 13:31:02 +0530

    Added singleton example

--- Creational/Creational.Singleton/Program.cs
+++ Creational/Creational.Singleton/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
+
+public class UserManager
+{
+    private static readonly Lazy<UserManager> _instance = new Lazy<UserManager>(()=>new UserManager());
+    public static UserManager Instance => _instance.Value;
+
+    public IEnumerable<User> Users { get; set; } = Enumerable.Empty<User>();
+    private UserManager()
+    {
+        Users = new List<User>
+        {
+            new(){ Id = 1, Name = "Subrahmanya"},
+            new(){ Id = 1, Name = "Ananth"}
+        };
+    }
+
+    public void PrintUsers()
+    {
+        foreach (var user in Users)
+        {
+            Console.WriteLine($"{user.Name}");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        UserManager.Instance.PrintUsers();
+    }
+}
